f19e786f1bbd79c85a8451c9b4c99d32251ab155
Alan Dsilva <alandsilva2001@gmail.com>
2021-03-20 19:44:53 +0530

    Update

---
+++ menu.js
@@ -0,0 +1,349 @@
+let projectData = [
+  {
+    name: "Room 1",
+    id: "room-1",
+    projects: [
+      {
+        name: "Safety Wristband",
+        url:
+          "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8c86b9bd9_5_3",
+      },
+      {
+        name: "Cowarrior: Sanitization And Alert System",
+        url:
+          "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc88d60eae1_0_10",
+      },
+      {
+        name: "Watchdog",
+        url:
+          "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc88d60eae1_0_15",
+      },
+      {
+        name: "Face Mask Detection Using Ai and Iot",
+        url:
+          "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc88d60eae1_0_23",
+      },
+      {
+        name: "Smart Bin",
+        url:
+          "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc88d60eae1_0_26",
+      },
+      {
+        name: "Smart Parking Solutions For On-street And Off-street Parking",
+        url:
+          "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc88d60eae1_0_29",
+      },
+    ],
+  },
+  {
+    name: "Room 2",
+    id: "room-2",
+    projects: [
+      {
+        name: "Smart shopping with Smart Trolley",
+        url:
+          "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_60",
+      },
+      {
+        name: "Deep Learning - Plant Species",
+        url:
+          "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_74",
+      },
+      {
+        name: "Motion Based Smart Device For Differently Abled People",
+        url:
+          "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_88",
+      },
+      {
+        name: "Home Automation Center",
+        url:
+          "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_102",
+      },
+      {
+        name: "Modernization of AGV by RFID",
+        url:
+          "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_116",
+      },
+      {
+        name: "Drishti 2.0",
+        url:
+          "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_130",
+      },
+    ],
+  },
+  {
+    name: "Room 3",
+    id: "room-3",
+    projects: [
+      {
+        name: "Automatic Sanitizer Dispenser",
+        url:
+          "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_183",
+      },
+      {
+        name: "IOT based Smart Social Distancing Device",
+        url:
+          "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_197",
+      },
+      {
+        name: "Plant Tree Redeem Points Application",
+        url:
+          "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_211",
+      },
+      {
+        name: "Hybrid Solar Powered Inverter",
+        url:
+          "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_225",
+      },
+      {
+        name: "D-monitor",
+        url:
+          "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_239",
+      },
+      {
+        name:
+          "Mems Multi Sensor Intelligence Damage Detection For Wind Turbines Using Iot",
+        url:
+          "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_253",
+      },
+    ],
+  },
+  {
+    name: "Room 4",
+    id: "room-4",
+    projects: [
+      {
+        name: "Simulation of Auto Nav of uav",
+        url:
+          "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_306",
+      },
+      {
+        name:
+          "Efficient power generation to automate street lights based on traffic density",
+        url:
+          "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_320",
+      },
+      {
+        name: "INMAP Navigator",
+        url:
+          "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_334",
+      },
+      {
+        name: "Wireless EV Charger Using MDC",
+        url:
+          "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_348",
+      },
+      {
+        name: "Field Monitoring Autonomous Drone",
+        url:
+          "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_362",
+      },
+      {
+        name: "Portable Inexpensive Potentiostat",
+        url:
+          "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_376",
+      },
+    ],
+  },
+  {
+    name: "Room 5",
+    id: "room-5",
+    projects: [
+      {
+        name: "Green Weathering Tiles",
+        url:
+          "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_429",
+      },
+      {
+        name: "Shower Buddy",
+        url:
+          "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_443",
+      },
+      {
+        name: "Application of Non-Humanoid robot in the field of hospitality",
+        url:
+          "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_457",
+      },
+      {
+        name: "Music Catalogue Management System",
+        url:
+          "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_471",
+      },
+      {
+        name:
+          "Novel soft start Improved efficiency single phase Induction blower motor Drive",
+        url:
+          "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_485",
+      },
+      //   {
+      //     name: "",
+      //     url:
+      //       "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_499",
+      //   },
+    ],
+  },
+  //   {
+  //     name: "Room 6",
+  //     id: "room-6",
+  //     projects: [
+  //       {
+  //         name: "Smart shopping with Smart Trolley",
+  //         url:
+  //           "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_60",
+  //       },
+  //       {
+  //         name: "Deep Learning - Plant Species",
+  //         url:
+  //           "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_74",
+  //       },
+  //       {
+  //         name: "Motion Based Smart Device For Differently Abled People",
+  //         url:
+  //           "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_88",
+  //       },
+  //       {
+  //         name: "Home Automation Center",
+  //         url:
+  //           "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_102",
+  //       },
+  //       {
+  //         name: "Modernization of AGV by RFID",
+  //         url:
+  //           "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_116",
+  //       },
+  //       {
+  //         name: "Drishti 2.0",
+  //         url:
+  //           "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_130",
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     name: "Room 7",
+  //     id: "room-7",
+  //     projects: [
+  //       {
+  //         name: "Smart shopping with Smart Trolley",
+  //         url:
+  //           "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_60",
+  //       },
+  //       {
+  //         name: "Deep Learning - Plant Species",
+  //         url:
+  //           "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_74",
+  //       },
+  //       {
+  //         name: "Motion Based Smart Device For Differently Abled People",
+  //         url:
+  //           "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_88",
+  //       },
+  //       {
+  //         name: "Home Automation Center",
+  //         url:
+  //           "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_102",
+  //       },
+  //       {
+  //         name: "Modernization of AGV by RFID",
+  //         url:
+  //           "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_116",
+  //       },
+  //       {
+  //         name: "Drishti 2.0",
+  //         url:
+  //           "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_130",
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     name: "Room 8",
+  //     id: "room-8",
+  //     projects: [
+  //       {
+  //         name: "Smart shopping with Smart Trolley",
+  //         url:
+  //           "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_60",
+  //       },
+  //       {
+  //         name: "Deep Learning - Plant Species",
+  //         url:
+  //           "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_74",
+  //       },
+  //       {
+  //         name: "Motion Based Smart Device For Differently Abled People",
+  //         url:
+  //           "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_88",
+  //       },
+  //       {
+  //         name: "Home Automation Center",
+  //         url:
+  //           "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_102",
+  //       },
+  //       {
+  //         name: "Modernization of AGV by RFID",
+  //         url:
+  //           "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_116",
+  //       },
+  //       {
+  //         name: "Drishti 2.0",
+  //         url:
+  //           "https://docs.google.com/presentation/d/e/2PACX-1vQz-_A_MVOquHX91C1kSYsdqd92oeYj2HPDbgqBdtb0XKFNkCQDdk86qrnxRtYFx1G9FolS2ug2Iely/embed?start=false&loop=false&delayms=3000&slide=id.gc8e433cffd_0_130",
+  //       },
+  //     ],
+  //   },
+];
+
+$(document).ready(function () {
+  projectData.forEach((item, index) => {
+    $("#menu-dropdown").append(group(item, index));
+  });
+
+  projectData.forEach((item, index) => {
+    let roomId = `#list-${item.id}`;
+    item.projects.forEach((item, index) => {
+      $(roomId).append(getListItem(item, index));
+    });
+  });
+});
+
+group = (item, index) => {
+  console.log(item);
+
+  let collapse = false;
+  if (index == 0) {
+    collapse = true;
+  }
+
+  return `
+    <div class="group">
+            <div class="menu-header" id="${item.id}">
+              <p
+                class="collapse-button"
+                type="button"
+                data-toggle="collapse"
+                data-target="#collapse-${item.id}"
+                aria-expanded="${collapse}"
+                aria-controls="collapse-${item.id}"
+              >
+                ${item.name}&nbsp;<i class="fa fa-chevron-down"></i>
+              </p>
+            </div>
+
+            <div
+              id="collapse-${item.id}"
+              class="collapse ${collapse ? "show" : ""}"
+              aria-labelledby="heading${index}"
+              data-parent="#menu-dropdown"
+            >
+              <div class="card">
+              <div class="card-body">
+              <ul id="list-${item.id}" class="list-group">
+              </ul>
+              </div>
+              </div>
+            </div>
+          </div>`;
+};
+
+getListItem = (item, index) => {
+  return `<li class="list-group-item"><a href="${item.url}" target="_blank">${item.name}</a></li>`;
+};
